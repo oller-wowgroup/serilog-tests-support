acebed380f720318f2296e8b3a77090beed4a1a7
Adam Voss <vossad01@gmail.com>
2015-06-28 17:54:26 -0500

    Adds solution and project for the former contents of Serilog.Tests/Support

---
+++ README.md
@@ -0,0 +1,5 @@
+# Serilog.Tests.Support
+
+Assertions and supporting classes for testing Serilog and its extensions.
+
+**Package** - [Serilog.Tests.Support]
---
+++ src/Serilog.Tests.Support/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Serilog.Tests.Support")]
+[assembly: AssemblyDescription("Assertions and supporting classes for testing Serilog and its extensions")]
+[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2015")]
